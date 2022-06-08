731ebf30d8c15ca66eadc31984312c4204f00e22
STRnick <nicksumin97@naver.com>
2022-06-08 16:21:02 +0900

    modify

--- sql/board.sql
+++ sql/board.sql
@@ -3,8 +3,28 @@
 desc board;
 desc user;
 
+select * from user;
+select * from board;
+
+delete from board;
+ALTER TABLE board AUTO_INCREMENT=1;
+
 select b.no, b.title, u.name, b.hit, date_format(b.reg_date, '%Y-%m-%d %H:%h:%s') as reg_date
 from board b, user u
+where b.user_no = u.no;
+
+select b.g_no, b.title, b.contents
+from board b, user u
 where b.no = u.no;
+
+select btitle, contents
+from board, user
+where g_no=1;
+
+delete from board
+where no=4;
+
+insert into board values(null, '테스트', '테스트', 0, now(), 1, 2, 1, 1);
+insert into board values(null, '테스트', '테스트', 0, now(), (select ifnull(max(g_no), 0)+1 from board b), 1, 1, 1);
 
 
