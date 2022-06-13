a9c22fff97eb7292700d33c6d4659e8d1f1c2541
STRnick <nicksumin97@naver.com>
2022-06-13 14:12:27 +0900

    AuthInterceptor + @Auth

--- mysite03/src/main/java/com/douzone/mysite/controller/BoardController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/BoardController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.douzone.mysite.security.Auth;
 import com.douzone.mysite.service.BoardService;
 import com.douzone.mysite.vo.BoardVo;
 import com.douzone.mysite.vo.UserVo;
@@ -38,75 +39,54 @@ public class BoardController {
 		model.addAttribute("boardVo", boardVo);
 		return "board/view";
 	}
-
+	
+	@Auth
 	@RequestMapping("/delete/{no}")
 	public String delete(HttpSession session, @PathVariable("no") Long boardNo, @RequestParam(value = "p", required = true, defaultValue = "1") Integer page, @RequestParam(value = "kwd", required = true, defaultValue = "") String keyword) {
-
 		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/board";
-		}
-		
 		boardService.deleteContents(boardNo, authUser.getNo());
 		return "redirect:/board?p=" + page + "&kwd=" + WebUtil.encodeURL(keyword, "UTF-8");
 	}
 
+	@Auth
 	@RequestMapping(value = "/modify/{no}")
 	public String modify(HttpSession session, @PathVariable("no") Long no, Model model) {
-
 		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/board";
-		}
-	
+		
 		BoardVo boardVo = boardService.getContents(no, authUser.getNo());
 		model.addAttribute("boardVo", boardVo);
 		return "board/modify";
 	}
 
+	@Auth
 	@RequestMapping(value = "/modify", method = RequestMethod.POST)
 	public String modify(HttpSession session, BoardVo boardVo, @RequestParam(value = "p", required = true, defaultValue = "1") Integer page, @RequestParam(value = "kwd", required = true, defaultValue = "") String keyword) {
-
 		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/board";
-		}
 		
 		boardVo.setUser_no(authUser.getNo());
 		boardService.modifyContents(boardVo);
 		return "redirect:/board/view/" + boardVo.getNo() + "?p=" + page + "&kwd=" + WebUtil.encodeURL(keyword, "UTF-8");
 	}
 
-	@RequestMapping(value = "/write", method = RequestMethod.GET)
-	public String write(HttpSession session) {
-
-		if(session.getAttribute("authUser") == null) {
-			return "redirect:/board";
-		}
-		
+	@Auth
+	@RequestMapping(value="/write", method = RequestMethod.GET)
+	public String write() {
 		return "board/write";
 	}
 
+	@Auth
 	@RequestMapping(value = "/write", method = RequestMethod.POST)
 	public String write(HttpSession session, BoardVo boardVo, @RequestParam(value = "p", required = true, defaultValue = "1") Integer page, @RequestParam(value = "kwd", required = true, defaultValue = "") String keyword) {
-
 		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/board";
-		}
-		
+
 		boardVo.setUser_no(authUser.getNo());
 		boardService.addContents(boardVo);
 		return "redirect:/board?p=" + page + "&kwd=" + WebUtil.encodeURL(keyword, "UTF-8");
 	}
 
+	@Auth
 	@RequestMapping(value = "/reply/{no}")
-	public String reply(HttpSession session, @PathVariable("no") Long no, Model model) {
-
-		if(session.getAttribute("authUser") == null) {
-			return "redirect:/board";
-		}
-		
+	public String reply(@PathVariable("no") Long no, Model model) {		
 		BoardVo boardVo = boardService.getContents(no);
 		boardVo.setO_no(boardVo.getO_no() + 1);
 		boardVo.setDepth(boardVo.getDepth() + 1);
