112dc48af2d23305c1c7f8fac47ca8490fc92486
STRnick <nicksumin97@naver.com>
2022-06-16 14:53:14 +0900

    mysite04 config migration #1

---
+++ mysite04/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.douzone.mysite.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.douzone.mysite.service.SiteService;
+import com.douzone.mysite.vo.SiteVo;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	private SiteService siteService;
+	
+	@RequestMapping({"/", "/main"})
+	public String index(Model model) {
+		// 나중에 두개 인터셉트에서 처리해야함
+		SiteVo vo = siteService.getSite();
+		model.addAttribute("site", vo);
+		
+		return "main/index";
+	}
+
+}
