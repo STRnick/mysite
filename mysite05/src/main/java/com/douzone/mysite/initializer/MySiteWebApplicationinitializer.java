aab26a7db46b143aa5ac31110d0f96b6cc204f81
STRnick <nicksumin97@naver.com>
2022-06-17 13:42:22 +0900

    NoHandlerFoundException 받기

---
+++ mysite05/src/main/java/com/douzone/mysite/initializer/MySiteWebApplicationinitializer.java
@@ -0,0 +1,43 @@
+package com.douzone.mysite.initializer;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.FrameworkServlet;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.douzone.mysite.config.app.AppConfig;
+import com.douzone.mysite.config.web.WebConfig;
+
+public class MySiteWebApplicationinitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+//		int[] a = new int[] {1, 2, 3, 4, 5};
+		return new Class<?>[] {AppConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		return new Filter[] {new CharacterEncodingFilter("UTF-8")};
+	}
+
+	@Override
+	protected FrameworkServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+		DispatcherServlet servlet = (DispatcherServlet) super.createDispatcherServlet(servletAppContext);
+		servlet.setThrowExceptionIfNoHandlerFound(true);
+		
+		return servlet;
+	}
+}
