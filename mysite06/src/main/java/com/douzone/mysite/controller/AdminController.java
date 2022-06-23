ae215a480d22cc7d967c47c92e9c887bd463495a
STRnick <nicksumin97@naver.com>
2022-06-23 11:25:47 +0900

    mysite06 #2

---
+++ mysite06/src/main/java/com/douzone/mysite/controller/AdminController.java
@@ -0,0 +1,62 @@
+package com.douzone.mysite.controller;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.douzone.mysite.security.Auth;
+import com.douzone.mysite.service.FileUploadService;
+import com.douzone.mysite.service.SiteService;
+import com.douzone.mysite.vo.SiteVo;
+
+@Auth(role="ADMIN")
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
+	@Autowired
+	private ServletContext servletContext;
+	
+	@Autowired
+	private SiteService siteService;
+	
+	@Autowired
+	private FileUploadService fileUploadService;
+
+	@RequestMapping("")
+	public String main() {
+		return "admin/main";
+	}
+
+	@RequestMapping(value = "/main/update", method = RequestMethod.POST )
+	public String update(SiteVo siteVo, @RequestParam("file") MultipartFile multipartFile) {
+		String url = fileUploadService.restore(multipartFile);
+		siteVo.setProfileURL(url);
+		
+		siteService.updateSite(siteVo);
+		servletContext.setAttribute("site", siteVo);
+
+		return "redirect:/admin";
+	}
+
+	@RequestMapping("/guestbook")
+	public String guestbook() {
+		return "admin/guestbook";
+	}
+
+	@RequestMapping("/board")
+	public String board() {
+		return "admin/board";
+	}
+
+	@RequestMapping("/user")
+	public String user() {
+		return "admin/user";
+	}
+
+}
